fa5f333e8f3193c441dd5ba832fb40d0bb6c9168
Maria <parody@uw.edu>
2015-08-05 16:12:50 -0700

    bananas no longer appear on top of each other

--- Assets/Scripts/Experiment/FoodController.cs
+++ Assets/Scripts/Experiment/FoodController.cs
@@ -1,45 +1,91 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 
 public class FoodController : MonoBehaviour {
-
 	public GameObject spawnBanana;
-	public GameObject spawnCarrot;
 	public GameObject spawnCherry;
 	public GameObject player;
-	public int numFruit;
-
+	public int numBananas;
+	public int numCherries;
+	public float distThresh;
+	
+	private List<GameObject> exists; //food and the player that exist in the world
+	
 	void Start(){
-		SpawnFood (spawnBanana);
+		exists = new List<GameObject> ();
+		exists.Add(player);
 	}
-
+	
 	void Update()
 	{
 		GameObject [] left = GameObject.FindGameObjectsWithTag ("Food");
 		if (left.Length <=0) {
-			print ("None");
-			SpawnFood(spawnBanana); //TODO: switch to other fruit when necessary
+			SpawnBananas();
+			SpawnCherries();
 		}
 	}
 
-	void SpawnFood(GameObject foodToSpawn){
-		foodToSpawn.SetActive(true);
-		foodToSpawn.tag = "Food";
+	void SpawnBananas(){
+		SpawnSet (spawnBanana, numBananas);
+	}
+
+	void SpawnCherries(){
+		SpawnSet (spawnCherry, numCherries);
+	}
+
+	void SpawnSet(GameObject foodToSpawn, int numFruit){
+		emptyList();
 		for (int i = 0; i<numFruit; i++) {
-			SpawnObject(foodToSpawn, i); //set ID number to i
+			SpawnObject(foodToSpawn, i);
 		}
 	}
-
+	
 	void SpawnObject(GameObject foodToSpawn, int ID){
-		float x = Random.Range (-9f, 9f);
-		float z = Random.Range (-9f, 9f);
-		Instantiate(foodToSpawn, new Vector3(x,.5f,z), foodToSpawn.transform.rotation);
+		bool keepThis;
+		bool itfits = false;
+		float x;
+		float z;
+		int maxIt = 100000;
+		int it = 0;
 
-		float randomRotation = Random.Range (0.0f, 360.0f);
-		foodToSpawn.transform.RotateAround (transform.position, Vector3.up, randomRotation);
+		do {
+			do {
+				keepThis = true;
+				x = Random.Range (-9f, 9f);
+				z = Random.Range (-9f, 9f);
+				for (int i = 0; i<exists.Count; i++) {
+					if (Distance (exists [i].transform.position.x, exists [i].transform.position.z, x, z) < distThresh) {
+						keepThis = false;
+					}
+				}
+				it++;
+			} while(keepThis == false && it<maxIt);
+			if (!keepThis){
+				it = 0;
+				distThresh /=2;
+				print (distThresh);
+			}else{
+				itfits = true;
+			}
+		} while(itfits == false);
+		GameObject spawnedObj  = Instantiate(foodToSpawn, new Vector3(x,.5f,z), foodToSpawn.transform.rotation) as GameObject;
+		exists.Add (spawnedObj);
 
-		foodToSpawn.GetComponent<Food> ().SetIDNum (ID);
+		float randomRotation = Random.Range(0.0f, 360.0f);
+		spawnedObj.transform.RotateAround(spawnedObj.transform.position, Vector3.up, randomRotation);
+
+		spawnedObj.GetComponent<Food>().SetIDNum(ID);
 	}
+	
+	float Distance(float x1, float z1, float x2, float z2){
+		return Mathf.Sqrt(Mathf.Pow (x1 - x2,2)+Mathf.Pow (z1-z2,2));
+	}
+	
+	void emptyList(){
+		exists.Clear ();
+		exists.Add (player);
+	}
+
 		
 }
