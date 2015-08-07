1012f90c3b096bbc821631a15d54f1a13a4f3ea3
Maria <mariak@mariakathryn.net>
2015-08-07 13:50:28 -0700

    brought in new dll for reward, getting ready to test.

--- Assets/Scripts/NationalInstruments/GiveReward.cs
+++ Assets/Scripts/NationalInstruments/GiveReward.cs
@@ -3,15 +3,15 @@ using System.Collections;
 using System.Runtime.InteropServices;
 
 public class GiveReward : MonoBehaviour {
-	[DllImport ("ASimplePlugin")]
-	private static extern int PrintANumber();
+	[DllImport ("NidaqPlugin")]
+	private static extern int reward(bool on);
 
-	[DllImport ("ASimplePlugin")]
-	private static extern int reward(int on);
+	//[DllImport ("ASimplePlugin")]
+	//private static extern int reward(int on);
 	
 	// Use this for initialization
 	void Start () {
-		Debug.Log(PrintANumber());
+		//Debug.Log(PrintANumber());
 		// Debug.Log (reward (true));
 		//Debug.Log (reward (0));
 	}
@@ -19,10 +19,10 @@ public class GiveReward : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(KeyCode.Z)){
-			Debug.Log (reward (1));
+			Debug.Log (reward (true));
 		}
 		else if(Input.GetKeyDown(KeyCode.X)){
-			Debug.Log (reward (0));
+			Debug.Log (reward (false));
 		}
 	}
 }
