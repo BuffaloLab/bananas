0f9ac4758c2988bab2156c20c68f3abd45d42af3
conovich <conovich@gmail.com>
2015-08-10 17:19:44 -0400

    Replay should now happen with an FPS specified in the experiment class.

--- Assets/Scripts/Experiment/Experiment.cs
+++ Assets/Scripts/Experiment/Experiment.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 //EXTEND OTHER EXPERIMENT SUBCLASSES FROM HERE FOR EASE OF USE IN REPLAY/ETC.
 public abstract class Experiment : MonoBehaviour {
+	public int ReplayFramesPerSecond = 60;
 	public bool isReplay;
 	public bool isSavingToPng;
 	public string PNGRecordingPath;
