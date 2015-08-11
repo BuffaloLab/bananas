d15b64052131d35ecbb6f67e12abe27484a662df
Maria <parody@uw.edu>
2015-08-11 16:21:11 -0700

    have callback working from c++ to unity, but c++ callback not working yet

--- Assets/Scripts/NationalInstruments/GiveReward.cs
+++ Assets/Scripts/NationalInstruments/GiveReward.cs
@@ -5,45 +5,59 @@ using System.Runtime.InteropServices;
 using System.Reflection;
 
 public class GiveReward : MonoBehaviour {
-
-	//[DllImport ("NidaqPlugin")]
-	//private static extern int reward(int on);
-
-	static int reward (int on) {
-		return on;
-	}
-
-	//[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-	//public delegate void set_callback(IntPtr data);
-
-	//[DllImport ("NidaqPlugin")]
-	//private static extern int eog_set_callback (
-	//	[MarshalAs(UnmanagedType.FunctionPtr)]set_callback 
-	//		eog_callback);
 	
-	//[DllImport ("ASimplePlugin")]
-	//private static extern int reward(int on);
+	[DllImport ("NidaqPlugin")]
+	private static extern int reward(int on);
+
+	//static int reward (int on) {
+	//	return on;
+	//}
+
+	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+	public delegate void set_callback_del(IntPtr callback);
+
+	[DllImport ("NidaqPlugin")]
+	private static extern int eog_set_callback (
+	[MarshalAs(UnmanagedType.FunctionPtr)]set_callback_del 
+			eye_data);
 	
+	[DllImport ("NidaqPlugin")]
+	private static extern int eog_start_task();
+	
+	[DllImport ("NidaqPlugin")]
+	private static extern int eog_stop_task();
+
 	// Use this for initialization
-	void Start () {
-		//eog_set_callback (eye_data);
+	void Start () 
+	{
+		set_callback_del callback =
+			(data) =>
+				{	
+				Debug.Log ("made callback");
+				Debug.Log (data);
+				};
+		Debug.Log ("start task");
+		Debug.Log (eog_start_task ());	
+		Debug.Log ("set callback");
+		Debug.Log (eog_set_callback (callback));
+		Debug.Log ("callback set");
 		//Debug.Log(PrintANumber());
 		// Debug.Log (reward (true));
 		//Debug.Log (reward (0));
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		if(Input.GetKeyDown(KeyCode.Z)){
+			Debug.Log ("reward on");
 			Debug.Log (reward (1));
 		}
 		else if(Input.GetKeyDown(KeyCode.X)){
+			Debug.Log ("stop reward");
 			Debug.Log (reward (0));
+			Debug.Log ("stop task");
+			Debug.Log (eog_stop_task());
 		}
-	}
-
-	void eye_data (int data) {
-		Debug.Log ("made callback");
-		Debug.Log (data);
 	}
 }
