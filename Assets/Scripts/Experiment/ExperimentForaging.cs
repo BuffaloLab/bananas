dbf16781dd7f6d6ce59624a7609454c4b68c485e
yonibrowning <yonib@uw.edu>
2015-08-11 14:58:55 -0700

    Updated Foraging Experiment to Update using OnFoodCollisionDelegate instead of GetComponentsWithTag

--- Assets/Scripts/Experiment/ExperimentForaging.cs
+++ Assets/Scripts/Experiment/ExperimentForaging.cs
@@ -4,20 +4,26 @@ using System.Collections;
 public class ExperimentForaging : Experiment {
 
 	public MonkeyConfig ChosenMonkey_Foraging;
+	public GameObject player;
+	int remaining = 0; //This will need to be set from the config file, once we get there
 
 	// Use this for initialization
 	void Start () {
-		
+		player.GetComponent<AvatarControls>().OnFoodCollisionDelegate += removeOne;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (!isReplay) {
-			GameObject [] left = GameObject.FindGameObjectsWithTag ("Food");
-			if (left.Length <= 0) {
-				myFoodController.SpawnBananas ();
-				myFoodController.SpawnCherries ();
-			}
+		if (!isReplay && remaining<=0) {
+			myFoodController.SpawnBananas ();
+			myFoodController.SpawnCherries ();
+			GameObject[] toCount = GameObject.FindGameObjectsWithTag("Food");
+			remaining = toCount.Length;
 		}
+		print (remaining);
+	}
+
+	void removeOne(){
+		remaining--;
 	}
 }
