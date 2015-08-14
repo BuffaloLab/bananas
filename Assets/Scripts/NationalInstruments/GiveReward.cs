384f3cc09cb246c3c0debd647de38a2932d87811
Maria <parody@uw.edu>
2015-08-14 13:48:35 -0700

    much better handle on how callbacks work. Still trying to figure out why readanalog call is not working. try making a simpler call back to c++. I think problem is data not being an array.

--- Assets/Scripts/NationalInstruments/GiveReward.cs
+++ Assets/Scripts/NationalInstruments/GiveReward.cs
@@ -13,59 +13,83 @@ public class GiveReward : MonoBehaviour {
 	//	return on;
 	//}
 
+	// this defines the callback (delegate) that we will be
+	// calling from the C code
+	// something not right nere. have tried both int and intptr for taskhandle
 	[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-	public delegate void set_callback_del(IntPtr callback);
+	public delegate void eog_callback_del(IntPtr taskHandle,
+	                                      Int32 everyNSamplesEventType,
+	                                      UInt32 nSamples,
+	                                      IntPtr callbackData);
 	//public delegate void set_callback_del(IntPtr callback);
 
 	[DllImport ("NidaqPlugin")]
 	private static extern int eog_set_callback (
-	[MarshalAs(UnmanagedType.FunctionPtr)]set_callback_del 
+	[MarshalAs(UnmanagedType.FunctionPtr)]eog_callback_del 
 			eye_data);
 	
 	[DllImport ("NidaqPlugin")]
-	private static extern IntPtr eog_start_task();
+	private static extern Int32 eog_start_task();
 	
 	[DllImport ("NidaqPlugin")]
 	private static extern int eog_stop_task();
 
+	// have tried interleaved (DAQmx_Val_GroupByScanNumber) as both IntPtr and Int32,
+	// considered bool32 in C code, whatever the hell that is suppose to be. Okay, bool32
+	// is zero or one, like bool, but is 32bits. bool is 8 bits, so I think making this 
+	// int32 is the way to go. both interleaved and reserved are bool32, however reserved
+	// requires an input of null, and I can only do this if I make it int?, as far as I can tell
 	[DllImport ("NidaqPlugin")]
 	private static extern float DAQmxReadAnalogF64 (
-		int taskHandle,
+		IntPtr taskHandle,
 		Int32 sampsPerChan,
 		float timeout,
-		IntPtr interleaved,
+		Int32 interleaved,
 		float data,
 		UInt32 sizeArray,
 		Int32 read,
 		int? reserved);
 
-	public int numSamples = 1;
-	public IntPtr DAQmx_Val_GroupByScanNumber;
+	public Int32 numSamples = 1;
+	public Int32 DAQmx_Val_GroupByChannel;
 	public Int32 read;
+	// in c++ this is data[1] seems most likely this one is the problem.
+	// what the hell should this be?!?!?
+	// try using float data; in c++ code in example callback and see if it 
+	// barfs in not array.
 	public float data;
 
 	// Use this for initialization
 	void Start () 
 	{
-		set_callback_del callback =
-			(status) =>
-				{	
-				Debug.Log ("made callback");
-				//Debug.Log (DAQmxReadAnalogF64(0,
-			    //                          numSamples,
-			    //                          1.0f,
-			    //                          DAQmx_Val_GroupByScanNumber,
-			    //                          data,
-			    //                          1,
-			    //                          read,
-			    //                          null));
-
-			     Debug.Log (status);
-				};
+		// this defines the callback that is called from the C++
+		// code. 
+		eog_callback_del set_callback =
+			(taskHandle, everyNSamplesEventType, nSamples, callbackData) =>
+			{	
+			Debug.Log ("made callback");
+			Debug.Log (taskHandle);
+			Debug.Log (everyNSamplesEventType);
+			Debug.Log (nSamples);
+			Debug.Log (callbackData);
+			Debug.Log (numSamples);
+			Debug.Log (DAQmx_Val_GroupByChannel);
+			Debug.Log (data);
+			Debug.Log (read);
+			Debug.Log (DAQmxReadAnalogF64(taskHandle,
+			                              numSamples,
+			                              10.0f,
+			                              DAQmx_Val_GroupByChannel,
+			                              data,
+			                              1,
+			                              read,
+			                              null));
+			Debug.Log ("read data");
+			};
 		Debug.Log ("start task");
-		Debug.Log (DAQmx_Val_GroupByScanNumber = eog_start_task ());	
+		Debug.Log (DAQmx_Val_GroupByChannel = eog_start_task ());
 		Debug.Log ("set callback");
-		Debug.Log (eog_set_callback (callback));
+		Debug.Log (eog_set_callback (set_callback));
 		Debug.Log ("callback set");
 		//Debug.Log(PrintANumber());
 		// Debug.Log (reward (true));
